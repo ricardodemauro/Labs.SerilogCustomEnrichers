99f03bbfd834be370df6381b76d9cb12916e6d8b
Ricardo de Mauro <c-ricardo.demauro@charter.com>
2023-04-12 13:40:14 -0400

    Add project files.

---
+++ src/RmauroLabs.SerilogSeries.EnrichContext/Program.cs
@@ -0,0 +1,53 @@
+using Serilog;
+using Serilog.Context;
+using Serilog.Events;
+
+LogEventProperty[] NoProperties = new LogEventProperty[0];
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Default", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{RequestScheme}://{RequestHost} [{ClientIp}] {Message:lj}{NewLine}{Exception}")
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    app.Use(async (httpContext, next) =>
+    {
+        var ctxHost = LogContext.PushProperty("RequestHost", httpContext.Request.Host.HasValue ? httpContext.Request.Host.Value : string.Empty);
+        var ctxScheme = LogContext.PushProperty("RequestScheme", httpContext.Request.Scheme);
+        var ctxCientIp = LogContext.PushProperty("ClientIp", httpContext.Connection.RemoteIpAddress);
+
+        await next();
+
+        ctxCientIp.Dispose();
+        ctxHost.Dispose();
+        ctxScheme.Dispose();
+    });
+
+    app.MapGet("/", (ILogger<Program> logger) =>
+    {
+        logger.LogInformation("Running Hello World");
+        return Results.Ok("Hello World!");
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
